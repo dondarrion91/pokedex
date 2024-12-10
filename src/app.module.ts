3d1c311d1cf4734d5e1cc0c2ce8f2a83d3014432
dondarrion91 <julisanchez1991@gmail.com>
2024-12-10 00:34:26 -0300

    Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
+import { SeedModule } from './seed/seed.module';
+import { CommonModule } from './common/common.module';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://172.21.0.2:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsdb',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
